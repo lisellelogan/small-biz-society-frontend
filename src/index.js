a7278284812a54cbef6d02b84080cf7749fabef5
lisellelogan <liselle.logan@yahoo.com>
2021-04-24 13:42:04 -0400

    update html and create variables to manipulate DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 fetch('http://localhost:3000/small_business_items')
   .then(response => response.json())
   .then(data => console.log(data));
+
+ const smallBizList = document.getElementById('small-biz-list')
+
+ const addSmallBizItem = document.getElementById('add-small-biz-item')
