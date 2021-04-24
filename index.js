4c9dfcca749665c65e503a2ed42e086bf708b982
lisellelogan <liselle.logan@yahoo.com>
2021-04-24 13:53:45 -0400

    create button to add new item

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ fetch('http://localhost:3000/small_business_items')
   .then(data => console.log(data));
 
  const smallBizList = document.getElementById('small-biz-list')
-
  const addSmallBizItem = document.getElementById('add-small-biz-item')
+ const addSmallBizItemBtn = document.getElementById('add-small-biz-button')
