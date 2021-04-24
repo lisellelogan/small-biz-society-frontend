7edeed10fc202db13d7eb176f57921799f601d15
lisellelogan <liselle.logan@yahoo.com>
2021-04-24 15:28:02 -0400

    plan function and create function to render small businesses

--- src/smallBusinessApi.js
+++ src/smallBusinessApi.js
@@ -1,8 +1,14 @@
 class SmallBusinessApi {
 
+    // getSmallBusinesses => make fetch request to /small_businesses
+    // call renderSmallBusinesses
     static getSmallBusinesses(){
         fetch('http://localhost:3000/small_businesses')
         .then(response => response.json())
         .then(data => console.log(data));
+    }
+
+    renderSmallBusinesses(){
+        //create DOM nodes and insert data into nodes to display in the DOM
     }
 }
