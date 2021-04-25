10dbb5afb1ca864bc10707949e5878d0829cd0a4
lisellelogan <liselle.logan@yahoo.com>
2021-04-25 13:14:55 -0400

    grab list to add small biz items

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const smallBizList = document.getElementById('small-biz-list')
 const addSmallBiz= document.getElementById('add-small-biz')
 const addSmallBizBtn = document.getElementById('add-small-biz-button')
-
+const smallBizItemList = document.getElementById('small-biz-item-list')
+const addSmallBizItem = document.getElementById('add-small-biz-item')
+const addSmallBizItemBtn = document.getElementById('add-small-biz-item-button')
 //add even listener for addSmallBizBtn
 //when clicked, will display form
 //once form is submitted, form will disappear
