a8f0d8901d4d743c74b0bf8b34acb74bfcc1281a
lisellelogan <liselle.logan@yahoo.com>
2021-04-25 12:06:10 -0400

    push new object into all array

--- src/smallBusinessItem.js
+++ src/smallBusinessItem.js
@@ -1,5 +1,9 @@
 class SmallBusinessItem {
-    constructor(id, name, price, description, image_url, type_of_product, small_business_id){
+
+    //use static to specify is class variable
+    static all = []
+
+    constructor({id, name, price, description, image_url, type_of_product, small_business_id}){
         this.id = id
         this.name = name
         this.price = price
@@ -7,5 +11,7 @@ class SmallBusinessItem {
         this.image_url = image_url
         this.type_of_product = type_of_product
         this.small_business_id = small_business_id
+
+        SmallBusinessItem.all.push(this)
     }
 }
