98a2c4305ef8d5133ff899b1077e2266b148bf62
lisellelogan <liselle.logan@yahoo.com>
2021-04-25 15:16:32 -0400

    move form event listener in smallBusiness.js

--- index.js
+++ index.js
@@ -4,45 +4,7 @@ const addSmallBizBtn = document.getElementById('add-small-biz-button')
 const smallBizItemList = document.getElementById('small-biz-item-list')
 const addSmallBizItem = document.getElementById('add-small-biz-item')
 const addSmallBizItemBtn = document.getElementById('add-small-biz-item-button')
-//add even listener for addSmallBizBtn
-//when clicked, will display form
-//once form is submitted, form will disappear
-addSmallBizBtn.addEventListener('click', handleBizBtnEvent)
 
-function handleBizBtnEvent(e){
-  //remove the button when clicked
-  addSmallBizBtn.remove()
-  //display form
-  addSmallBiz.innerHTML = `
-  <form id="small-biz-form">
-    <label for="biz-name">Biz Name: </label><br>
-    <input type="text" name="name" id="new-biz-name"></input><br>
-    <label for="biz-price-range">Price Range: </label><br>
-    <input type="float" name="price_range" id="new-biz-price-range"></input><br>
-    <label for="biz-address">Address: </label><br>
-    <input type="text" name="address" id="new-biz-address"></input><br>
-    <input id="create-small-biz" type="submit" value="Add My Small Biz"></input>
-  </form>
-  `
-//add EventListener to form
-//submit form => form disappears
-//prevent the default post request
-const smallBizForm = document.getElementById('small-biz-form')
-smallBizForm.addEventListener("submit", handleSubmit)
-}
-
-function handleSubmit(e){
-  e.preventDefault()
-  //grab value from input field
-  const newBizName = document.getElementById('new-biz-name')
-  const newBizPriceRange = document.getElementById('new-biz-price-range')
-  const newBizAddress = document.getElementById('new-biz-address')
-  //add new biz to list
-  //reset the page so form is gone
-  SmallBusinessItemApi.createSmallBizItem()
-  smallBizForm.reset()
-  //add button back
-}
 
 SmallBusinessItemApi.getSmallBusinessesItems()
 
--- src/smallBusiness.js
+++ src/smallBusiness.js
@@ -12,6 +12,8 @@ class SmallBusiness {
         this.li.id = `small-biz-${this.id}`
         this.li.dataset.id = this.id
 
+        addSmallBizBtn.addEventListener('click', this.handleBizBtnEvent)
+
         SmallBusiness.all.push(this)
     }
 
@@ -24,6 +26,39 @@ class SmallBusiness {
         smallBizList.append(this.addBizToLi())
     }
 
-    //make function where you click on add small biz button and form appears
+    handleBizBtnEvent(){
+        //remove the button when clicked
+        addSmallBizBtn.remove()
+        //display form
+        addSmallBiz.innerHTML = `
+        <form id="small-biz-form">
+            <label for="biz-name">Biz Name: </label><br>
+            <input type="text" name="name" id="new-biz-name"></input><br>
+            <label for="biz-price-range">Price Range: </label><br>
+            <input type="float" name="price_range" id="new-biz-price-range"></input><br>
+            <label for="biz-address">Address: </label><br>
+            <input type="text" name="address" id="new-biz-address"></input><br>
+            <input id="create-small-biz" type="submit" value="Add My Small Biz"></input>
+        </form>
+        `
+        //add EventListener to form
+        //submit form => form disappears
+        //prevent the default post request
+        const smallBizForm = document.getElementById('small-biz-form')
+        smallBizForm.addEventListener("submit", this.handleSmallBizSubmit)
+    }
+
+    handleSmallBizSubmit(e){
+        e.preventDefault()
+        //grab value from input field
+        // const newBizName = document.getElementById('new-biz-name')
+        // const newBizPriceRange = document.getElementById('new-biz-price-range')
+        // const newBizAddress = document.getElementById('new-biz-address')
+        //add new biz to list
+        //reset the page so form is gone
+        SmallBusinessItemApi.createSmallBizItem()
+        smallBizForm.reset()
+        //add button back 
+    }
 
 }
