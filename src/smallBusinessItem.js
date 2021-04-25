c650dd3e33480b1dd821d21a17190731acfbd76d
lisellelogan <liselle.logan@yahoo.com>
2021-04-25 11:52:25 -0400

    create constructor

---
+++ src/smallBusinessItem.js
@@ -0,0 +1,11 @@
+class SmallBusinessItem {
+    constructor(id, name, price, description, image_url, type_of_product, small_business_id){
+        this.id = id
+        this.name - name
+        this.price = price
+        this.description = description
+        this.image_url = image_url
+        this.type_of_product = type_of_product
+        this.small_business_id = small_business_id
+    }
+}
