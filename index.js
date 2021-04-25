c89bc3c2379b19f036e50e601fcb42779db1b857
lisellelogan <liselle.logan@yahoo.com>
2021-04-24 20:15:38 -0400

    add click event to add small business

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
- const smallBizList = document.getElementById('small-biz-list')
- const addSmallBiz= document.getElementById('add-small-biz')
- const addSmallBizBtn = document.getElementById('add-small-biz-button')
+const smallBizList = document.getElementById('small-biz-list')
+const addSmallBiz= document.getElementById('add-small-biz')
+const addSmallBizBtn = document.getElementById('add-small-biz-button')
 
+//add even listener for addSmallBizBtn
+//when clicked, will display form
+//once form is submitted, form will disappear
+addSmallBizBtn.addEventListener('click', handleBizBtnEvent)
 
- 
+function handleBizBtnEvent(e){
+  //remove the button when clicked
+  //display form
+  //submit form => form disappears
+  
+}
 
 SmallBusinessApi.getSmallBusinesses()
