db2a3927f0c50811daf972a7688807fa08277d87
lisellelogan <liselle.logan@yahoo.com>
2021-04-25 11:30:06 -0400

    grab input values from form

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function handleBizBtnEvent(e){
   addSmallBiz.innerHTML = `
   <form id="small-biz-form">
     <label for="biz-name">Biz Name: </label><br>
-    <input type="text" name="name" id="new-biz"></input><br>
+    <input type="text" name="name" id="new-biz-name"></input><br>
     <label for="biz-price-range">Price Range: </label><br>
     <input type="float" name="price_range" id="new-biz-price-range"></input><br>
     <label for="biz-address">Address: </label><br>
@@ -32,6 +32,9 @@ smallBizForm.addEventListener("submit", handleSubmit)
 function handleSubmit(e){
   e.preventDefault()
   //grab value from input field
+  const newBizName = document.getElementById('new-biz-name')
+  const newBizPriceRange = document.getElementById('new-biz-price-range')
+  const newBizAddress = document.getElementById('new-biz-address')
   //add new biz to list
   //reset the page so form is gone
   //add button back
