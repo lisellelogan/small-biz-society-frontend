a7b35254b8277f44322116400973b76e43c63136
lisellelogan <liselle.logan@yahoo.com>
2021-04-25 11:58:24 -0400

    fix name = name

--- src/smallBusinessItem.js
+++ src/smallBusinessItem.js
@@ -1,7 +1,7 @@
 class SmallBusinessItem {
     constructor(id, name, price, description, image_url, type_of_product, small_business_id){
         this.id = id
-        this.name - name
+        this.name = name
         this.price = price
         this.description = description
         this.image_url = image_url
